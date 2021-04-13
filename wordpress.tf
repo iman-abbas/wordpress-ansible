bda20879610501bc77d3d821d3ff3aadabbecd5f
Iman abbas <iman.abbas@tothenew.com>
2021-04-13 12:48:40 +0530

    first commit

---
+++ wordpress.tf
@@ -0,0 +1,223 @@
+provider "aws" {
+  region     = "ap-south-1"
+
+}
+
+
+#creating_vpc
+resource "aws_vpc" "wordpressvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "wordpressvpc"
+    purpose = "wordpress"
+  }
+enable_dns_hostnames = "true"
+}
+
+#creating_public_subnet
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.wordpressvpc.id
+  cidr_block = "10.0.1.0/24"
+ availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "public-subnet"
+    purpose = "wordpress"
+  }
+}
+
+#creating_private_subnet
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.wordpressvpc.id
+  cidr_block = "10.0.2.0/24"
+availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private"
+    purpose = "wordpress"
+  }
+}
+
+#creating_securitygroup
+
+resource "aws_security_group" "wordpresstask-sg" {
+  name        = "wordpresstask-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.wordpressvpc.id
+ 
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  tags = {
+    Name = "wordpress-sg"
+    purpose = "wordpress"
+  }
+ 
+}
+ 
+#creating_internet-gateway
+
+resource "aws_internet_gateway" "mywpigw" {
+  vpc_id = aws_vpc.wordpressvpc.id
+ 
+  tags = {
+    Name = "mywpigw"
+    purpose = "wordpress"
+  }
+}
+
+#creating_route_table
+ 
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.wordpressvpc.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mywpigw.id
+  }
+  tags = {
+    Name = "public-rt"
+    purpose = "wordpress"
+  }
+}
+
+#associating_route_table
+
+resource "aws_route_table_association" "publicassociation" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+#creating_keypair
+
+resource "aws_key_pair" "wordpresskeys" {
+  key_name   = "wordpresskeys"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/WMntpPZbwO3gr0iP2arwQo8GKlEwNaO+8oHiOgai+sQK/bVO9RjcppozfFVROycYz2yJ+POFquH/aB7f5RccgkkR6JzK0qx8O0e7X5ET7nfKTDu0SHlxczuZ0QGSdHpzaDL7SHiWQkf6vRx0c0w4bYDV9BgyHzjp5H9vRx9cAl1YYCsTglBfG2vj0++tT955t6PE/StCXtPdCDNYPrgWvO2wRZIWRYHAH7NPeOnlTcfFPBRL+4KBIrBHL4uF+5thfERE1n0/c9VZ/Jc7wExFS5qL+Q4Ne3wLqbH3Z5fPKOO8VoZwBvHTJmVlH9BOITohuh9yHiUJk+tQKsYgzG7J ttn@TTNPL"
+}
+
+#creating_asg
+
+resource "aws_launch_configuration" "mylaunchconfiguration" {
+  image_id               = "ami-0bcf5425cdc1d8a85"
+  instance_type          = "t2.micro"
+  security_groups        = [aws_security_group.wordpresstask-sg.id]
+
+  key_name               = "wordpresskeys"
+  user_data = file("script.sh")
+
+  lifecycle {
+    create_before_destroy = true
+  }
+associate_public_ip_address = true
+}
+
+#Creating AutoScaling Group
+
+resource "aws_autoscaling_group" "myasg" {
+  launch_configuration = aws_launch_configuration.mylaunchconfiguration.id
+ 
+  
+ vpc_zone_identifier  = [aws_subnet.public-subnet.id]
+  min_size = 1
+  max_size = 2
+  desired_capacity          = 1
+  health_check_type = "ELB"
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# autoscaling group attachment
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.myasg.id
+  alb_target_group_arn   = aws_lb_target_group.wordpress-tg.arn
+}
+
+#Creating load-balancer
+
+resource "aws_lb" "web_alb" {
+  name = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_security_group.wordpresstask-sg.id
+  ]
+  subnets = [
+    aws_subnet.public-subnet.id,
+    aws_subnet.private-subnet.id
+  ]
+  tags = {
+    purpose = "wordpress"
+  }
+  # cross_zone_load_balancing   = true
+  
+}
+
+  # target group
+  resource "aws_lb_target_group" "wordpress-tg" {
+  name     = "tf-wordpress-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.wordpressvpc.id
+}
+
+  resource "aws_lb_listener" "listener" {
+    load_balancer_arn = aws_lb.web_alb.arn
+    port = 80
+    protocol = "HTTP"
+
+     default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress-tg.arn
+  }
+  }
+
+
+resource "aws_db_subnet_group" "sb-grp" {
+  name       = "main"
+  subnet_ids = [aws_subnet.public-subnet.id, aws_subnet.private-subnet.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+# Launching RDS db instance
+
+resource "aws_db_instance" "myrds" {
+  allocated_storage    = 20
+  max_allocated_storage = 100
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "wordpress"
+  username             = "iman"
+  password             = "iman12345"
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = [aws_security_group.wordpresstask-sg.id]
+
+  availability_zone = "ap-south-1a"
+  publicly_accessible = false
+  db_subnet_group_name = aws_db_subnet_group.sb-grp.id
+  
+  skip_final_snapshot = true 
+
+}
